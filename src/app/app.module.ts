2a4bbae5882aa44775bd36919ec6cff67ade85f0
narjes <nrjsharafi@gmail.com>
2022-11-22 08:24:29 +0330

    initialize project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { SubserviceComponent } from './subservice/subservice.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent,
+    SubserviceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/subservice/subservice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-subservice',
+    templateUrl: 'subservice.component.html',
+    styleUrls: ['subservice.component.scss']
+})
+
+export class SubserviceComponent{
+
+}
